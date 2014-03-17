56773215b304495bdb415a65bcfb6231cad8b853
Dylan Foster <fosterdill@gmail.com>
2014-03-17 02:16:01 -0700

    add masonry callback after adding index view  to DOM, start writing #render function

--- js/app/routers/router.js
+++ js/app/routers/router.js
@@ -13,14 +13,20 @@ Beaches.Routers.Router = Backbone.Router.extend({
       data: Beaches.params,
       success: function () {
         var indexView = new Beaches.Views.Index();
-        that._swapView(indexView);
+        that._swapView(indexView, indexView.initializeMasonry);
       }
     });
   },
 
-  _swapView: function (view) {
+  //helper function that helps avoid view event listener leaks
+  _swapView: function (view, callback) {
     this.currentView && this.currentView.remove();
     this.currentView = view;
     this.$rootEl.html(view.render().$el);
+    
+    //necessary to call method on view after masonry el rendered to
+    //DOM, masonry#appended won't work unless already on DOM, need
+    //to pass callback written in view
+    if (callback) callback.call(view);
   }
 });
--- js/app/views/index.js
+++ js/app/views/index.js
@@ -2,8 +2,27 @@ Beaches.Views.Index = Backbone.View.extend({
   initialize: function () {
     var source = $('#photo').html();
     this.template = Handlebars.compile(source);
+    this.masonryOptions = {
+      columnWidth: 200,
+      itemSelector: '.photo'
+    };
   },
 
   render: function () {
-  }
+    Beaches.album.each( this.addPhoto.bind(this) );
+    return this;
+  },
+
+  addPhoto: function (photo) {
+    
+    //add photo to masonry container using handlebar template
+    var newContent = this.template({ 
+      thumbnail: photo.get('thumbnail')
+    });
+    this.$el.append(newContent);
+  },
+
+  initializeMasonry: function () {
+    this.$el.masonry(this.masonryOptions);
+  },
 });
