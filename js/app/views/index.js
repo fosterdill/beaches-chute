f22db6d8fa3c5f5d3f412d04c06e5fd7048d8047
Dylan Foster <fosterdill@gmail.com>
2014-03-17 02:23:46 -0700

    Begin work with masonry in view, write method and listener for when user needs more photos

--- js/app/views/index.js
+++ js/app/views/index.js
@@ -6,6 +6,7 @@ Beaches.Views.Index = Backbone.View.extend({
       columnWidth: 200,
       itemSelector: '.photo'
     };
+    this.listenTo(Beaches.album, 'add', this.addPhotoWithMasonry);
   },
 
   render: function () {
@@ -13,13 +14,25 @@ Beaches.Views.Index = Backbone.View.extend({
     return this;
   },
 
+  //appends to DOM and calls masonry's appended method
+  addPhotoWithMasonry: function (photo) {
+    var newContent = this.renderTemplate(photo);
+    var photoElement = $(newContent).get(0);
+    this.$el.append(photoElement).masonry('appended', photoElement);
+  },
+
   addPhoto: function (photo) {
     
     //add photo to masonry container using handlebar template
-    var newContent = this.template({ 
-      thumbnail: photo.get('thumbnail')
-    });
+    //does not use appended method. Only used for initial pull
+    var newContent = this.renderTemplate(photo);
     this.$el.append(newContent);
+  },
+
+  renderTemplate: function (photo) {
+    return this.template({ 
+      thumbnail: photo.get('thumbnail') 
+    });
   },
 
   initializeMasonry: function () {
